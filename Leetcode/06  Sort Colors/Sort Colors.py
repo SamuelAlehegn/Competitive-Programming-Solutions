77e63e70b57eb1063a4ec50bb8608896da6444e7
SamuelAlehegn <hadis.samuelalehegn@gmail.com>
2023-04-18 15:34:37 +0300

    Leetcode: Sort colors

--- Leetcode/06  Sort Colors/Sort Colors.py
+++ Leetcode/06  Sort Colors/Sort Colors.py
@@ -3,6 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
+        In-Place Sorting Algorithms:
+                    Bubble sort, 
+                    Selection Sort,
+                    Insertion Sort, 
+                    Heapsort.
+        """
+        # Bubble Sort
         list_len = len(nums)
         for i in range(list_len):
             for j in range(list_len - i - 1):
